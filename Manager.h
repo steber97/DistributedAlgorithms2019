ec01bcb6a7083e63e97b61d8c6642a3f13f2fecc
Stefano <stefano.huber@gmail.com>
2019-11-01 23:45:45 +0100

    General change of directory structure

--- Manager.h
+++ Manager.h
@@ -1,52 +1,14 @@
-#ifndef PROJECT_TEMPLATE_MANAGER_H
-#define PROJECT_TEMPLATE_MANAGER_H
+//
+// Created by stefano on 01/11/19.
+//
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <time.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <vector>
-#include <queue>
-#include <unordered_map>
-#include <mutex>
-#include <iostream>
-#include <pthread.h>
-#include <thread>
-#include <condition_variable>
+#ifndef DISTRIBUTEDALGORITHMS2019_MANAGER_H
+#define DISTRIBUTEDALGORITHMS2019_MANAGER_H
 
-
-using namespace std;
-
-// This is set to true at the beginning of the program.
-// As soon as we receive the signal to start the program, then we can set it to false
-// and all processes can start sending proper messages instead of initialization messages.
-// the access to this variable is protected by a mutex
-
-extern mutex mtx_initialization_phase;
-extern bool initialization_phase;
 
 class Manager {
-    vector<int> ports;
-    vector<char*> ips;
-    vector<int> processes;
-    vector<int> sockfd;
-    int process_number;
-    int number_of_messages;
 
-public:
-    Manager(vector<int> &ports, vector<char *> &ips, vector<int> &processes, int process_number,
-            int number_of_messages);        // Constructor
-
-    unordered_map<int,int> port_id;
-
-    void run();
 };
 
 
-#endif //PROJECT_TEMPLATE_MANAGER_H
+#endif //DISTRIBUTEDALGORITHMS2019_MANAGER_H
---
+++ old_structure/utilities.h
@@ -0,0 +1,17 @@
+
+#ifndef PROJECT_TEMPLATE_UTILITIES_H
+#define PROJECT_TEMPLATE_UTILITIES_H
+
+#include "Manager.h"
+
+/**
+ * this function returns a string (const char*) out of an int
+ * @param n: any integer value (I guess only positive ones).
+ * @return
+ */
+const char* int_to_char_pointer(int n);
+
+Manager* parse_input_data(int argc, char** argv);
+
+#endif //PROJECT_TEMPLATE_UTILITIES_H
+
