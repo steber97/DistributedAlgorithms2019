4e5dd33dc9cf0a243754f9dfce9fd96d7bb59a1a
Stefano <stefano.huber@gmail.com>
2019-11-03 18:01:01 +0100

    Found way to kill thread when message arrive

---
+++ thread_kill.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <thread>
+#include <condition_variable>
+#include <mutex>
+#include <future>
+#include <chrono>
+#include <iostream>
+#include <vector>
+
+//using namespace std::literals::chrono_literals;
+
+struct timer_killer {
+    // returns false if killed:
+    template<class R, class P>
+    bool wait_for( std::chrono::duration<R,P> const& time ) {
+        std::unique_lock<std::mutex> lock(m);
+        return !cv.wait_for(lock, time, [&]{return terminate;});
+    }
+    void kill() {
+        std::unique_lock<std::mutex> lock(m);
+        terminate=true;
+        cv.notify_all();
+    }
+private:
+    std::condition_variable cv;
+    std::mutex m;
+    bool terminate = false;
+};
+
+timer_killer bob;
+
+void wait(){
+    bool a = true;
+    while(a){
+        std::cout << "another loop" << std::endl;
+        a = bob.wait_for(std::chrono::milliseconds(1000));
+    }
+}
+
+int main() {
+
+    std::thread t(wait);
+    std::cout << "killing threads\n";
+
+    int a;
+    std::cin >> a;
+
+    bob.kill();
+
+    t.join();
+    std::cout << "done\n";
+    // your code goes here
+    return 0;
+}
