1d5cf2d92f477515ab8de45204cfa8e81a860a3a
Stefano <stefano.huber@gmail.com>
2019-11-06 17:55:47 +0100

    Use variable instead of numeric constant in validate.sh

--- validate_perfect_link.sh
+++ validate_perfect_link.sh
@@ -2,9 +2,10 @@
 
 init_time=2
 time_to_finish=20
+number_of_processes=3
 
 # start 5 processes, each broadcasting 100 messages
-for i in `seq 1 5`
+for i in `seq 1 $number_of_processes`
 do
     ./da_proc $i membership &
     da_proc_id[$i]=$!
@@ -12,7 +13,7 @@ done
 
 sleep $init_time
 
-for i in `seq 1 5`
+for i in `seq 1 $number_of_processes`
 do
     if [ -n "${da_proc_id[$i]}" ]; then
 	kill -USR2 "${da_proc_id[$i]}"
@@ -22,14 +23,14 @@ done
 sleep $time_to_finish
 
 # stop all processes (don't know why, but we have to stop them twice)
-for i in `seq 1 5`
+for i in `seq 1 $number_of_processes`
 do
     if [ -n "${da_proc_id[$i]}" ]; then
 	kill -TERM "${da_proc_id[$i]}"
     fi
 done
 
-for i in `seq 1 5`
+for i in `seq 1 $number_of_processes`
 do
     if [ -n "${da_proc_id[$i]}" ]; then
 	kill "${da_proc_id[$i]}"
