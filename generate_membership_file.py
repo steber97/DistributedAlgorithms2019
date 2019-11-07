d6f9b120e5552744dcc0c324db17ce808d4b7f4e
Stefano <stefano.huber@gmail.com>
2019-11-07 18:42:04 +0100

    Add small description for python script

--- generate_membership_file.py
+++ generate_membership_file.py
@@ -1,5 +1,13 @@
 import sys
 
+"""
+usage of the script:
+
+python3 generate_membership_file.py number_of_processes number_of_messages
+
+any other usage is going to result in an undefined behaviour.
+"""
+
 def pad_with_zero(i):
     if i<10:
         return "00" + str(i)
