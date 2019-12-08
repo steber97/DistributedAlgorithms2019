dba78787bece1b2f62a431ddc7bcf88d9d37ee86
Stefano <stefano.huber@gmail.com>
2019-12-08 17:28:08 +0100

    Add comments to bebroadcast

--- BeBroadcast.cpp
+++ BeBroadcast.cpp
@@ -8,20 +8,26 @@ BeBroadcast::BeBroadcast(Link* link, int number_of_processes, int number_of_mess
 }
 
 
+
+/**
+ * Initialization phase, it detaches a new thread which stays forever listening to
+ * new messages from perfect links.
+ */
 void BeBroadcast::init(){
-    // starts the deliverer
+    // starts the deliverer on a separate detached thread
     thread delivery_checker(run_deliverer_beb, this->link, this);
     delivery_checker.detach();
 }
 
 
 /**
- * The actual method that broadcast a message
- *
- * @param msg to be broadcasted
+ * Broadcasts a message to all processes.
+ * @param msg the message to broadcast.
  */
 void BeBroadcast::beb_broadcast(b_message &msg) {
     pp2p_message pp2p_msg = pp2p_message(false, this->link->get_process_number(), msg);
+
+    // Just send the message to every process.
     for (int i = 1; i <= number_of_processes; i++) {
         link->send_to(i, pp2p_msg);
     }
@@ -29,10 +35,8 @@ void BeBroadcast::beb_broadcast(b_message &msg) {
 
 
 /**
- * Method used when a message ha to be delivered, in fact it pushes
- * the message in the queue of the messages delivered at beb level
- *
- * @param msg message to be delivered
+ * Delivers a message to the upper layer (in this case, Urb).
+ * @param msg the message to beb deliver
  */
 void BeBroadcast::beb_deliver(b_message &msg) {
     // Put the message in the queue so that it can be delivered to urb.
@@ -48,14 +52,24 @@ void BeBroadcast::beb_deliver(b_message &msg) {
 }
 
 
+/**
+ * Reads concurrently the first message that can be beb_delivered.
+ * Read is concurrent as the thread detached with init() is the writer,
+ * and threads running the upper layers are the readers (actually only one, Urb)
+ * It can be blocking, if no messages are present in the queue, for instance,
+ * it waits using a condition variable, until there is a new message to beb deliver.
+ *
+ * @return the first message which has been beb_delivered
+ */
 b_message BeBroadcast::get_next_beb_delivered(){
+    // acquire the lock on the condition variable, shared with the thread running run_deliverer_beb
     unique_lock<mutex> lck(mtx_beb_urb);
     cv_beb_urb.wait(lck, [&] { return !queue_beb_urb.empty(); });
     queue_beb_urb_locked = true;
-    b_message msg = queue_beb_urb.front();
+    b_message msg = queue_beb_urb.front();  // push the message in the queue
     queue_beb_urb.pop();
     queue_beb_urb_locked = false;
-    cv_beb_urb.notify_one();
+    cv_beb_urb.notify_one();    // notify the other thread that the queue now is ready to be written in
 
     return msg;
 }
@@ -63,12 +77,14 @@ b_message BeBroadcast::get_next_beb_delivered(){
 
 
 /**
- * The deliverer only receives messages from the link level
- * using the method exposed by the link get_next_message.
- * It runs on a separate thread
+ * This method runs on a separate thread, just listens to incoming messages coming from
+ * the lower layer (perfect Link) and beb delivers them as soon as they arrive.
+ *
+ * Small caveat: sometimes perfect link sends garbage (it happens for instance during threads shutdown)
+ * When it happens, fake messages are sent.
  *
  * @param link
- * @param number_of_processes
+ * @param beb_broadcast
  */
 void run_deliverer_beb(Link* link, BeBroadcast* be_broadcast){
     while(true) {
@@ -77,6 +93,8 @@ void run_deliverer_beb(Link* link, BeBroadcast* be_broadcast){
         // is with same first_sender and seq number of the pp2p pp2p_message.
         if (!is_pp2p_fake(msg))   // only deliver it if it is not fake.
             be_broadcast->beb_deliver(msg.payload);
+        else
+            break;
     }
 }
 
--- BeBroadcast.h
+++ BeBroadcast.h
@@ -8,6 +8,8 @@
 
 /**
  * This class represent the best-effort broadcast abstraction
+ * It simply relies on the perfect link properties in order to fulfill
+ * validity, no duplication and no creation.
  */
 class BeBroadcast {
 private:
@@ -16,12 +18,48 @@ private:
     int number_of_messages;
 public:
     BeBroadcast(Link* link, int number_of_processes, int number_of_messages);
+
+    /**
+     * Initialization phase, it detaches a new thread which stays forever listening to
+     * new messages from perfect links.
+     */
     void init();
+
+    /**
+     * Broadcasts a message to all processes.
+     * @param msg the message to broadcast.
+     */
     void beb_broadcast(b_message &msg);
+
+    /**
+     * Delivers a message to the upper layer (in this case, Urb).
+     * @param msg the message to beb deliver
+     */
     void beb_deliver(b_message &msg);
+
+    /**
+     * Reads concurrently the first message that can be beb_delivered.
+     * Read is concurrent as the thread detached with init() is the writer,
+     * and threads running the upper layers are the readers (actually only one, Urb)
+     * It can be blocking, if no messages are present in the queue, for instance,
+     * it waits using a condition variable, until there is a new message to beb deliver.
+     *
+     * @return the first message which has been beb_delivered
+     */
     b_message get_next_beb_delivered();
 };
 
+
+/**
+ * This method runs on a separate thread, just listens to incoming messages coming from
+ * the lower layer (perfect Link) and beb delivers them as soon as they arrive.
+ *
+ * Small caveat: sometimes perfect link sends garbage (it happens for instance during threads shutdown)
+ * When it happens, fake messages are sent.
+ *
+ * @param link
+ * @param beb_broadcast
+ */
 void run_deliverer_beb(Link* link, BeBroadcast* beb_broadcast);
 
 
