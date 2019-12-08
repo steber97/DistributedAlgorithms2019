3e1bda7dd7b8f82f1f36d60a990ff6e99464514a
Stefano <stefano.huber@gmail.com>
2019-12-08 18:08:05 +0100

    Add comments to Fifo Broadcast

--- FifoBroadcast.cpp
+++ FifoBroadcast.cpp
@@ -8,23 +8,35 @@ bool fifo_delivering_queue_locked = false;   //
 
 FifoBroadcast::FifoBroadcast(UrBroadcast *urb, int number_of_processes) {
     this->urb = urb;
+
+    // next to deliver stores which is the next message we need to deliver for every process, in order to respect the FIFO property.
     this->next_to_deliver.resize(number_of_processes+1, 1);   // they are 1 plus the normal size, as we start counting by 1
     this->pending.resize(number_of_processes+1, unordered_set<int>());
     this->fifo_delivering_queue = new queue<lcob_message* >;  // Initialize the queue of messages to share with lcobroadcast
 }
 
-
+/**
+ * Detaches the thread that will receives messages retrieved from lower layers (URB).
+ */
 void FifoBroadcast::init() {
     thread t_urb_delivered_handler(handle_urb_delivered, this);
     t_urb_delivered_handler.detach();
 }
 
 
+
+/**
+ * Fifo broadcast a message, is actually the same as urb broadcasting it.
+ */
 void FifoBroadcast::fb_broadcast(b_message msg) {
     urb->urb_broadcast(msg);
 }
 
-
+/**
+ * Delivers a message to the upper layers
+ * (messages are pushed into a queue of messages which can be accessed concurrently).
+ * @param msg_to_deliver
+ */
 void FifoBroadcast::fb_deliver(b_message msg_to_deliver) {
     //urb_delivery_log(msg_to_deliver);
     unique_lock<mutex> lck(mtx_fifo_delivering_queue);
@@ -37,10 +49,22 @@ void FifoBroadcast::fb_deliver(b_message msg_to_deliver) {
     cv_fifo_delivering_queue.notify_all();
 }
 
+
+/**
+ * Gets next message coming from the lower layer (urb)
+ * @return
+ */
 b_message FifoBroadcast::get_next_urb_delivered() {
     return urb->get_next_urb_delivered();
 }
 
+
+/**
+ * method exposed for upper layers to get the next fifo delivered message.
+ * Can be blocking, when there is no message to deliver.
+ * Uses condition variables and notify in order not to waste resources doing busy waiting.
+ * @return
+ */
 lcob_message FifoBroadcast::get_next_fifo_delivered() {
     // returns messages to local causal order broadcast
     unique_lock<mutex> lck(mtx_fifo_delivering_queue);
@@ -63,14 +87,18 @@ lcob_message FifoBroadcast::get_next_fifo_delivered() {
 }
 
 /**
- * This is just a wrapper, so that both fifo and urb can be used to build on top of it lcob
- * @param msg_to_deliver
+ * Just an alias for fb_broadcast, so that it can be used by template class
  */
 void FifoBroadcast::broadcast(b_message msg_to_deliver) {
     // Just invokes fifo broadcast
     this->fb_broadcast(msg_to_deliver);
 }
 
+
+/**
+ * just an alias for get_next_fifo_delivered()
+ * @return
+ */
 lcob_message FifoBroadcast::get_next_delivered() {
     // this is just a wrapper for get_next_fifo_delivered,
     // so that it can be used by lcob_broadcast.
@@ -78,6 +106,26 @@ lcob_message FifoBroadcast::get_next_delivered() {
 }
 
 
+/**
+ * Decides when it is the case to deliver messages coming from the lower layers.
+ * It has to respect FIFO order, so whenever we receive messages from the same
+ * process but out of order, we need to wait and deliver first the remaining messages
+ * with a smaller sequence number.
+ *
+ * CAREFUL HERE!!
+ * We are doing a very silly assumption (which was told us by the TAs, so it is justifiable):
+ * whenever we urb_deliver a message m5 with sequence number 5 and coming from process p1,
+ * considering that we know that messages are broadcast in order, we can assume that whenever we
+ * receive a message out of sequence, even the other messages with lower sequence number have been
+ * broadcast in order, therefore we can deliver all messages up to the one we have received,
+ * even if, strictly speaking, we have never received messages with that sequence number.
+ *
+ * For instance, we have received m5, but the last sequence number we have delivered so far for process p1
+ * is 2. Then we can assume that process p1 has broadcast even 3 and 4 in the correct order, and therefore
+ * we can deliver 3, 4 and finally 5, respecting the FIFO order, even if we haven't received yet nor 3 nor 4.
+ *
+ * @param fb
+ */
 void handle_urb_delivered(FifoBroadcast *fb) {
     while (true) {
         b_message msg = fb->get_next_urb_delivered();
@@ -87,11 +135,12 @@ void handle_urb_delivered(FifoBroadcast *fb) {
         // a lower sequence number starting from the same sender.
         if (fb->next_to_deliver[msg.first_sender] <= msg.seq_number) {
             for (int i = fb->next_to_deliver[msg.first_sender]; i <= msg.seq_number; i++) {
+                // create an instance of the message to deliver, setting the correct sequence number
                 b_message msg_to_fifo_deliver(i, msg.first_sender, msg.lcob_m);
                 fb->fb_deliver(msg_to_fifo_deliver);
             }
 
-            fb->next_to_deliver[msg.first_sender] = msg.seq_number + 1;
+            fb->next_to_deliver[msg.first_sender] = msg.seq_number + 1;  // increment the local counter by 1
         }
     }
 }
